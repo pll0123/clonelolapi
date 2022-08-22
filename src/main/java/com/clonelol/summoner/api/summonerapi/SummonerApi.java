d2ea162968f60b217e9de6527e1826933f09b571
diaboloss217 <diaboloss217@gmail.com>
2022-08-23 01:10:54 +0900

    Feat: Masterleagues, Grandmasterleagues, Challengerleagues

--- src/main/java/com/clonelol/summoner/api/summonerapi/SummonerApi.java
+++ src/main/java/com/clonelol/summoner/api/summonerapi/SummonerApi.java
@@ -2,6 +2,8 @@ package com.clonelol.summoner.api.summonerapi;
 
 import com.clonelol.summoner.api.summonerapi.dto.SummonerApiDto;
 import com.clonelol.summoner.api.summonerapi.dto.SummonerIdInfoDto;
+import com.clonelol.summoner.api.summonerapi.dto.property.datas.LeagueItemData.LeagueItemDto;
+import com.clonelol.summoner.api.summonerapi.dto.property.datas.LeagueListDto;
 import com.clonelol.summoner.service.SummonerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,6 +29,7 @@ public class SummonerApi {
 
     String[] tiers = {"GOLD", "PLATINUM", "DIAMOND"};
     String[] divisions = {"I", "II", "III", "IV"};
+    String[] masterTiers = {"masterleagues", "grandmasterleagues", "challengerleagues"};
 
     @RequestMapping("/summoner")
     public void AutoSearch() throws InterruptedException {
@@ -34,6 +37,11 @@ public class SummonerApi {
             log.info("페이지 시작 : {}", page);
             searchSummonerApi(page, tiers[2], divisions[3]);
         }
+    }
+
+    @RequestMapping("/masters")
+    public void AutoMasterSearch() throws InterruptedException {
+        searchSummonerMasterApi(masterTiers[2]);
     }
 
     public void searchSummonerApi(int page, String tier, String division) throws InterruptedException {
@@ -78,6 +86,63 @@ public class SummonerApi {
                     .retrieve()
                     .bodyToMono(SummonerIdInfoDto.class)
                     .block()
+            );
+        }
+
+        for (int i = 0; i < summonerIDinfos.size(); i++) {
+            log.info("번호: {}", i);
+            log.info("SUMMONER_ID - {}", summonerIDinfos.get(i).getSummonerId());
+            log.info("PUUID - {}", summonerIDinfos.get(i).getPuuId());
+            log.info("------------------------------");
+        }
+        summonerService.initializeAllIdInfo(summonerIDinfos);
+
+    }
+
+    public void searchSummonerMasterApi(String MasterTiers) throws InterruptedException {
+        LeagueListDto leagueListDto = webClient
+                .baseUrl(BASE_KOR_API)
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(-1))
+                .build()
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/lol/league/v4/{MasterTiers}/by-queue/RANKED_SOLO_5x5")
+                        .queryParam("api_key", DEV_KEY)
+                        .build(MasterTiers))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(LeagueListDto.class)
+                .block();
+
+        System.out.println("여기까지 테스트");
+
+        List<String> summonerIds = leagueListDto.getEntries()
+                .stream()
+                .map(LeagueItemDto::getSummonerId)
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < summonerIds.size(); i++) {
+            log.info("summonersId {} : {}", i, summonerIds.get(i));
+        }
+
+        log.info("탐색된 유저 수 : {}", summonerIds.size());
+        List<SummonerIdInfoDto> summonerIDinfos = new ArrayList<>();
+        for (var Id : summonerIds) {
+            Thread.sleep(1500);
+            log.info("딜레이 : {}", Id);
+
+            summonerIDinfos.add(
+                    WebClient
+                            .builder()
+                            .baseUrl(BASE_KOR_API)
+                            .build()
+                            .get()
+                            .uri(uriBuilder -> uriBuilder.path(ENCRYPTED_SUMMONER_ID)
+                                    .queryParam("api_key",DEV_KEY)
+                                    .build(Id))
+                            .retrieve()
+                            .bodyToMono(SummonerIdInfoDto.class)
+                            .block()
             );
         }
 
